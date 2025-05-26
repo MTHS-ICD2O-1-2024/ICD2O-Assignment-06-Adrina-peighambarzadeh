73dff8d29d513aece96a3d43f99edcb409a8a67d
Adrina-peighambarzadeh <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-26 19:14:36 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Adrina peighambarzadeh All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adrina peighambarzadeh
+// Created on: June 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function gets a motivational quote from the API
+ * The 'async' is there because it will take time for the internet to return the value
+ */
+async function getMotivationalQuote () {
+  // the 'try' is here because the internet may not be working
+  try {
+    const resultJSON = await fetch(
+      'https://gomezmig03.github.io/MotivationalAPI/en.json'
+    )
+    const jsonData = await resultJSON.json()
+    console.log(jsonData)
+
+    // bring the information from API
+    const quoteText = jsonData[0].text
+    const quoteAuthor = jsonData[0].author
+
+    // output
+    // <b> format reference: 'https://www.w3schools.com/Jsref/jsref_bold.asp'
+    document.getElementById('motivational-quote').innerHTML =
+      '<b>Quote:</b> "' +
+      quoteText +
+      '"<br />' +
+      '<b>Author:</b> ' +
+      quoteAuthor +
+      '<br />'
+  } catch (error) {
+    console.error(error)
+  }
+}
